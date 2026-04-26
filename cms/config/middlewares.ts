be243cc3b34d146e69eb31712c71033973db04d6
Evi Wammes <e.wammes@amsterdam.nl>
2026-04-26 11:10:47 +0200

    Improve CORS policy

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -4,7 +4,15 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: (process.env.CORS_ORIGINS ?? 'http://localhost:3000').split(','),
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeadersOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
