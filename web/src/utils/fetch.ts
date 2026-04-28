4f27b69cf3c97caee51f5bfaf69b56b1d51958f0
Evi Wammes <e.wammes@amsterdam.nl>
2026-04-28 21:26:43 +0200

    chore: add placeholder env values for the build action

--- web/src/utils/fetch.ts
+++ web/src/utils/fetch.ts
@@ -4,7 +4,7 @@ const baseURL = process.env.NEXT_PUBLIC_STRAPI_URL
 const auth = process.env.STRAPI_API_TOKEN
 
 if (!baseURL || !auth) {
-  throw new Error('Missing enviorment variables')
+  throw new Error('Missing environment variables')
 }
 
 export const client = strapi({
