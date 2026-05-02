52c73b7894805d612b2ad5bac1d2aa9ff522b052
Evi Wammes <e.wammes@amsterdam.nl>
2026-05-02 11:49:41 +0200

    feat: update AppHeader to show name of user

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,7 +1,9 @@
 import Card from './card/Card'
 import LikeButton from './like-button/LikeButton'
+import EmojiPicker from './login/EmojiPicker'
+import LoginForm from './login/LoginForm'
 import AppHeader from './navigation/AppHeader'
 import Navigation from './navigation/Navigation'
 
-export { AppHeader, Card, LikeButton, Navigation }
+export { AppHeader, Card, EmojiPicker, LikeButton, LoginForm, Navigation }
 export default Navigation
--- ui/src/navigation/AppHeader.tsx
+++ ui/src/navigation/AppHeader.tsx
@@ -1,15 +1,27 @@
 import { PageHeader } from '@amsterdam/design-system-react'
 
-const AppHeader = () => (
+type AppHeaderProps = {
+  currentUser?: { emoji: string; name: string }
+}
+
+const AppHeader = ({ currentUser }: AppHeaderProps) => (
   <PageHeader
     brandName="Community Roadmap"
     menuItems={[
       <PageHeader.MenuLink fixed href="/zoeken" key="zoeken">
         Zoeken
       </PageHeader.MenuLink>,
-      <PageHeader.MenuLink href="/inloggen" key="inloggen">
-        Inloggen
-      </PageHeader.MenuLink>,
+      ...(currentUser
+        ? [
+            <PageHeader.MenuLink href="/uitloggen" key="uitloggen">
+              {currentUser.emoji} {currentUser.name}
+            </PageHeader.MenuLink>,
+          ]
+        : [
+            <PageHeader.MenuLink href="/inloggen" key="inloggen">
+              Inloggen
+            </PageHeader.MenuLink>,
+          ]),
     ]}
   />
 )
