248c7f1f29abcc7936a978c290f528ec87374285
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-02 15:11:21 +0200

    feat: Add functionality to like ideas

--- web/src/utils/strapi.ts
+++ web/src/utils/strapi.ts
@@ -53,8 +53,13 @@ export const strapi = {
       fetchParsed(`idea-likes/${id}`, strapiSingle(IdeaLikeSchema), init),
   },
   ideas: {
-    findMany: (init?: RequestInit) =>
-      fetchParsed('ideas?populate[likes][fields][0]=id', strapiCollection(IdeaSchema), init),
+    findMany: (init?: RequestInit) => {
+      const params = new URLSearchParams({
+        'populate[likes][fields][0]': 'documentId',
+        'populate[likes][populate][end_user][fields][0]': 'documentId',
+      })
+      return fetchParsed(`ideas?${params}`, strapiCollection(IdeaSchema), init)
+    },
     findOne: (id: string | number, init?: RequestInit) => fetchParsed(`ideas/${id}`, strapiSingle(IdeaSchema), init),
   },
   stories: {
