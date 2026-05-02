387618f5eec7dd3f6eb5710da8e69d839396e1e0
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-02 10:01:24 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@add4eb35a7df188398904c28c1e2ab43ad9b5747 🚀

---
+++ demo-login-system/assets/like-button.stories-CHwfujso.js
@@ -0,0 +1,19 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{o as t,t as n}from"./src-CKE04-V5.js";var r,i,a,o,s;e((()=>{n(),r={title:`LikeButton`,component:t,argTypes:{count:{control:{min:0,type:`number`}},isLiked:{control:`boolean`},size:{control:`select`,options:[`default`,`small`]}}},i={args:{count:25,isLiked:!1,size:`default`}},a={args:{count:25,isLiked:!0,size:`default`}},o={args:{count:7,isLiked:!1,size:`small`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 25,
+    isLiked: false,
+    size: 'default'
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 25,
+    isLiked: true,
+    size: 'default'
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 7,
+    isLiked: false,
+    size: 'small'
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`Liked`,`Small`]}))();export{i as Default,a as Liked,o as Small,s as __namedExportsOrder,r as default};
