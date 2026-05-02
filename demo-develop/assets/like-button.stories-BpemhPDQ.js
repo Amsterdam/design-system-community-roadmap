b1539f3ec60e85cc6bf7181c086394b5e13a324d
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-02 10:20:02 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@7691d5ca0204602da7494874365390976c360455 🚀

--- demo-develop/assets/like-button.stories-BpemhPDQ.js
+++ demo-develop/assets/like-button.stories-BpemhPDQ.js
@@ -1,4 +1,4 @@
-import{n as e}from"./chunk-DnJy8xQt.js";import{i as t,t as n}from"./src-CtMf0YZ1.js";var r,i,a,o,s;e((()=>{n(),r={title:`LikeButton`,component:t,argTypes:{count:{control:{min:0,type:`number`}},isLiked:{control:`boolean`},size:{control:`select`,options:[`default`,`small`]}}},i={args:{count:25,isLiked:!1,size:`default`}},a={args:{count:25,isLiked:!0,size:`default`}},o={args:{count:7,isLiked:!1,size:`small`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+import{n as e}from"./chunk-DnJy8xQt.js";import{l as t,t as n}from"./src-bEd501nC.js";var r,i,a,o,s;e((()=>{n(),r={title:`LikeButton`,component:t,argTypes:{count:{control:{min:0,type:`number`}},isLiked:{control:`boolean`},size:{control:`select`,options:[`default`,`small`]}}},i={args:{count:25,isLiked:!1,size:`default`}},a={args:{count:25,isLiked:!0,size:`default`}},o={args:{count:7,isLiked:!1,size:`small`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
   args: {
     count: 25,
     isLiked: false,
