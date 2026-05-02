e5baed448b489596a2125a18b480e05ed0b31e00
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-02 14:40:16 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@67e9084982eb065c1a374808da1b337ceb4a1f0d 🚀

---
+++ demo-dynamic-pages-setup/assets/card.stories-zCtNLEQr.js
@@ -0,0 +1,25 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{t,u as n}from"./src-BOTfxXnh.js";var r,i,a,o,s;e((()=>{t(),r={title:`Card`,component:n,argTypes:{isLiked:{control:`boolean`},variant:{control:`select`,options:[`big`,`small`]},voteCount:{control:{min:0,type:`number`}}}},i={args:{title:`Multi Select`,description:`In een component wil ik meerdere opties tegelijkertijd kunnen selecteren.`,isLiked:!1,variant:`big`,voteCount:25}},a={args:{title:`Loading UI`,description:`Hoe laat ik zien dat data nog wordt opgehaald in mijn applicatie?`,isLiked:!1,variant:`small`,voteCount:7}},o={args:{title:`Status Badge`,description:`Een badge om zelf een status te kunnen opzeggen van iets.`,isLiked:!0,variant:`big`,voteCount:24}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Multi Select',
+    description: 'In een component wil ik meerdere opties tegelijkertijd kunnen selecteren.',
+    isLiked: false,
+    variant: 'big',
+    voteCount: 25
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Loading UI',
+    description: 'Hoe laat ik zien dat data nog wordt opgehaald in mijn applicatie?',
+    isLiked: false,
+    variant: 'small',
+    voteCount: 7
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Status Badge',
+    description: 'Een badge om zelf een status te kunnen opzeggen van iets.',
+    isLiked: true,
+    variant: 'big',
+    voteCount: 24
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Big`,`Small`,`PreLiked`]}))();export{i as Big,o as PreLiked,a as Small,s as __namedExportsOrder,r as default};
