4219c51a95ca87763ddebeccf722c13d615603ca
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-25 10:47:48 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@d8682df70960f4d58ffc5bab9ce2118a6ded05ec 🚀

---
+++ demo-develop/assets/react-CqLIZxbq.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./DocsRenderer-LL677BLK-CdpS7vty.js";t();export{e as MDXProvider};
---
+++ demo-develop/assets/reactions.stories-B55zbrcn.js
@@ -0,0 +1,19 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{f as t,t as n}from"./src-D7JmjxSY.js";var r,i,a,o,s,c,l;e((()=>{n(),r={title:`Molecules/Reactions`,component:t,argTypes:{compact:{control:`boolean`},teamLabel:{control:`text`}}},i=[{author:{name:`Jan de Vries`},content:`Dit is een erg goed idee! Ik zou dit graag in de volgende versie zien.`,id:1},{author:{isTeam:!0,name:`Emma Bakker`},content:`We zijn hier al mee bezig als onderdeel van de nieuwe toegankelijkheidsrichtlijnen.`,id:2},{author:{name:`Pieter Post`},content:`Kan dit ook gebruikt worden in combinatie met andere componenten?`,id:3}],a={args:{reactions:i}},o={args:{compact:!0,reactions:i}},s={args:{reactions:[]}},c={args:{reactions:i,teamLabel:`DS Projectgroep`}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    reactions: mockReactions
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    compact: true,
+    reactions: mockReactions
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    reactions: []
+  }
+}`,...s.parameters?.docs?.source}}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    reactions: mockReactions,
+    teamLabel: 'DS Projectgroep'
+  }
+}`,...c.parameters?.docs?.source}}},l=[`Default`,`Compact`,`Empty`,`CustomTeamLabel`]}))();export{o as Compact,c as CustomTeamLabel,a as Default,s as Empty,l as __namedExportsOrder,r as default};
