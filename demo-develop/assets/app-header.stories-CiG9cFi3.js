2d7184e180a25f84523001caccd0f6ab5b2d7bce
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-25 13:59:32 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@781aea90b8fec2eeb0dc2bd3b9917018aa05f01a 🚀

---
+++ demo-develop/assets/app-header.stories-CiG9cFi3.js
@@ -0,0 +1,17 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{g as t,t as n}from"./src-Bgs4uQLm.js";var r,i,a,o,s;e((()=>{n(),r={title:`Organisms/App Header`,component:t,parameters:{layout:`fullscreen`}},i={},a={args:{currentUser:{emoji:`🦊`,name:`Jan de Vries`},onLogout:()=>{}}},o={args:{currentUser:{emoji:`🐼`,name:`Wilhelmina van den Berg`},onLogout:()=>{}}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    currentUser: {
+      emoji: '🦊',
+      name: 'Jan de Vries'
+    },
+    onLogout: () => {}
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    currentUser: {
+      emoji: '🐼',
+      name: 'Wilhelmina van den Berg'
+    },
+    onLogout: () => {}
+  }
+}`,...o.parameters?.docs?.source}}},s=[`LoggedOut`,`LoggedIn`,`LoggedInLongName`]}))();export{a as LoggedIn,o as LoggedInLongName,i as LoggedOut,s as __namedExportsOrder,r as default};
