6122ca60b636e0b276937a696e3955a4254afbd2
Evi-2003 <Evi-2003@users.noreply.github.com>
2026-05-27 19:01:19 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@60dfa94b64544783a036489f5a7ac04e59d8236c 🚀

---
+++ demo-ads-adoption/assets/card.stories-BQHDxGf2.js
@@ -0,0 +1,22 @@
+import{i as e}from"./preload-helper-xPQekRTU.js";import{t,w as n}from"./src-BR88GGkx.js";var r,i,a,o,s;e((()=>{t(),r={title:`Molecules/Card`,component:n,argTypes:{isLiked:{control:`boolean`},voteCount:{control:{min:0,type:`number`}}}},i={args:{title:`Multi Select`,description:`In een component wil ik meerdere opties tegelijkertijd kunnen selecteren.`,isLiked:!1,voteCount:25}},a={args:{title:`Loading UI`,description:`Hoe laat ik zien dat data nog wordt opgehaald in mijn applicatie?`,isLiked:!1,voteCount:7}},o={args:{title:`Status Badge`,description:`Een badge om zelf een status te kunnen opzeggen van iets.`,isLiked:!0,voteCount:24}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Multi Select',
+    description: 'In een component wil ik meerdere opties tegelijkertijd kunnen selecteren.',
+    isLiked: false,
+    voteCount: 25
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Loading UI',
+    description: 'Hoe laat ik zien dat data nog wordt opgehaald in mijn applicatie?',
+    isLiked: false,
+    voteCount: 7
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Status Badge',
+    description: 'Een badge om zelf een status te kunnen opzeggen van iets.',
+    isLiked: true,
+    voteCount: 24
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Big`,`Small`,`PreLiked`]}))();export{i as Big,o as PreLiked,a as Small,s as __namedExportsOrder,r as default};
