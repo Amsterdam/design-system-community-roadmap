21ac2037a5128f89249dacfee5f592612dc4d287
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-31 12:28:33 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@ccd4cc697445a372588ea2dccf98a7f92c0573c5 🚀

---
+++ demo-develop/assets/add-reaction.stories-BSnhn5mJ.js
@@ -0,0 +1,19 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{p as t,t as n}from"./src-BH2-q4ck.js";var r,i,a,o,s,c;e((()=>{n(),r={title:`Molecules/Add Reaction`,component:t,args:{onSubmit:()=>{}},argTypes:{onSubmit:{action:`submitted`}}},i={args:{isLoggedIn:!0}},a={args:{isLoggedIn:!1}},o={args:{isLoggedIn:!0,loading:!0}},s={args:{error:`Er is iets misgegaan bij het plaatsen van je reactie.`,isLoggedIn:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLoggedIn: true
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLoggedIn: false
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    isLoggedIn: true,
+    loading: true
+  }
+}`,...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    error: 'Er is iets misgegaan bij het plaatsen van je reactie.',
+    isLoggedIn: true
+  }
+}`,...s.parameters?.docs?.source}}},c=[`Default`,`NotLoggedIn`,`Loading`,`WithError`]}))();export{i as Default,o as Loading,a as NotLoggedIn,s as WithError,c as __namedExportsOrder,r as default};
---
+++ demo-develop/assets/like-button.stories-CyzHpwaj.js
@@ -0,0 +1,16 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{D as t,t as n}from"./src-BH2-q4ck.js";var r,i,a,o,s;e((()=>{n(),r={title:`Atoms/Like Button`,component:t,argTypes:{count:{control:{min:0,type:`number`}},isLiked:{control:`boolean`}}},i={args:{count:25,isLiked:!1}},a={args:{count:25,isLiked:!0}},o={args:{count:7,isLiked:!1}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 25,
+    isLiked: false
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 25,
+    isLiked: true
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    count: 7,
+    isLiked: false
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`Liked`,`Small`]}))();export{i as Default,a as Liked,o as Small,s as __namedExportsOrder,r as default};
