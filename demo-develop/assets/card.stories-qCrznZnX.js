b6f8b07c527b13960e85311f086f54dfb30bc071
Evi-2003 <69012052+Evi-2003@users.noreply.github.com>
2026-05-31 23:10:10 +0000

    Deploying to gh-pages from @ Amsterdam/design-system-community-roadmap@6b492c3f51583924581eeab02e1c71630f85c197 🚀

---
+++ demo-develop/assets/card.stories-qCrznZnX.js
@@ -0,0 +1,8 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{T as t,t as n}from"./src-GGVWosPP.js";var r,i,a;e((()=>{n(),r={title:`Molecules/Card`,component:t,argTypes:{isLiked:{control:`boolean`},voteCount:{control:{min:0,type:`number`}}}},i={args:{title:`Status Badge`,description:`Een badge om zelf een status te kunnen opzeggen van iets.`,isLiked:!0,voteCount:24}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: 'Status Badge',
+    description: 'Een badge om zelf een status te kunnen opzeggen van iets.',
+    isLiked: true,
+    voteCount: 24
+  }
+}`,...i.parameters?.docs?.source}}},a=[`PreLiked`]}))();export{i as PreLiked,a as __namedExportsOrder,r as default};
