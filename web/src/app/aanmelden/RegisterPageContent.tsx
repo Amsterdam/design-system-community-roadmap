1b53210e64cb687b0f21e4a4de5c28dcc018deeb
Evi Wammes <e.wammes@amsterdam.nl>
2026-05-31 12:00:44 +0200

    fix: improve layout of login page to improve signup flow

--- web/src/app/aanmelden/RegisterPageContent.tsx
+++ web/src/app/aanmelden/RegisterPageContent.tsx
@@ -1,18 +1,30 @@
 'use client'
 
-import { Grid, Heading, Paragraph } from '@amsterdam/design-system-react'
+import { Column, Grid, Heading, Paragraph, StandaloneLink } from '@amsterdam/design-system-react'
+import NextLink from 'next/link'
 
 import RegisterClient from './RegisterClient'
 
 export default function RegisterPageContent() {
   return (
     <Grid>
-      <Grid.Cell className="ams-prose" span={{ narrow: 4, medium: 6, wide: 6 }}>
+      <Grid.Cell className="ams-prose" span={{ narrow: 4, medium: 5, wide: 6 }}>
         <Heading level={1} size="level-2">
           Account aanmaken
         </Heading>
         <Paragraph>Kies je voornaam en een emoji.</Paragraph>
         <RegisterClient />
+      </Grid.Cell>
+      <Grid.Cell span={{ narrow: 4, medium: 3, wide: 4 }} start={{ narrow: 1, medium: 6, wide: 9 }}>
+        <Column gap="small">
+          <Heading level={2} size="level-4">
+            Al een account?
+          </Heading>
+          <Paragraph>Log in met je voornaam en emoji.</Paragraph>
+          <NextLink href="/inloggen" legacyBehavior passHref>
+            <StandaloneLink>Inloggen</StandaloneLink>
+          </NextLink>
+        </Column>
       </Grid.Cell>
     </Grid>
   )
--- web/src/app/inloggen/LoginPageContent.tsx
+++ web/src/app/inloggen/LoginPageContent.tsx
@@ -1,18 +1,30 @@
 'use client'
 
-import { Grid, Heading, Paragraph } from '@amsterdam/design-system-react'
+import { Column, Grid, Heading, Paragraph, StandaloneLink } from '@amsterdam/design-system-react'
+import NextLink from 'next/link'
 
 import LoginClient from './LoginClient'
 
 export default function LoginPageContent() {
   return (
     <Grid>
-      <Grid.Cell className="ams-prose" span={{ narrow: 4, medium: 6, wide: 6 }}>
+      <Grid.Cell className="ams-prose" span={{ narrow: 4, medium: 5, wide: 6 }}>
         <Heading level={1} size="level-2">
           Inloggen
         </Heading>
         <Paragraph>Vul je naam en emoji in om in te loggen.</Paragraph>
         <LoginClient />
+      </Grid.Cell>
+      <Grid.Cell span={{ narrow: 4, medium: 3, wide: 4 }} start={{ narrow: 1, medium: 6, wide: 9 }}>
+        <Column gap="small">
+          <Heading level={2} size="level-4">
+            Geen account?
+          </Heading>
+          <Paragraph>Maak eenmalig een account aan met je voornaam en een emoji.</Paragraph>
+          <NextLink href="/aanmelden" legacyBehavior passHref>
+            <StandaloneLink>Account aanmaken</StandaloneLink>
+          </NextLink>
+        </Column>
       </Grid.Cell>
     </Grid>
   )
